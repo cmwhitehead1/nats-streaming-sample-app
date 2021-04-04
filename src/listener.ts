af0ccdef7f5ce3be0190a9b0d094473fa8250369
Chris Whitehead <cdub@MacBook-Pro.attlocal.net>
2021-04-04 16:19:03 -0400

    - Randomly setting client id for scaling. - Adding options to the nats client. setManualAckMode(treu) - Logic to overrie the default NATS close logic.

--- src/listener.ts
+++ src/listener.ts
@@ -1,22 +1,67 @@
 import nats, { Message } from 'node-nats-streaming'
+import { randomBytes } from 'crypto'
+
+const clientID = randomBytes(4).toString('hex')
+const clientIdString = `ticket-listener-${clientID}`
 
 /**
  * Connect to the Nats streaming service
  */
-const stan = nats.connect('ticketing', 'ticket-listener', {
-  url: "http://localhost:4222"
+const stan = nats.connect('ticketing', clientIdString, {
+  url: 'http://localhost:4222',
 })
 
+/**
+ * Adding in Queue Groups will balance message delivery across a group of subscribers
+ * which can be used to provide application fault tolerance and scale workload processing.
+ * Helps so we don't double process an event.
+ */
 stan.on('connect', () => {
-  console.log('Listener connected to Nats..')
+  console.log('Listener connected to Nats: ', clientIdString)
 
-  const subscription = stan.subscribe('ticket:created');
+  /**
+   * On close kill the client.
+   * This is overriding the Nats default process for when 
+     clients are killed/closed. Nats would normally wait based on
+     params provided when started. (see params in nats-depl.yaml)
+   */
+  stan.on('close', () => {
+    console.log('Closing: ', clientIdString)
+    process.exit()
+  })
+
+  /**
+   * The setManualAckMode(true) option makes it so we have to
+   * manually make the acknowledge the we have received the event.
+   */
+  const options = stan.subscriptionOptions().setManualAckMode(true)
+
+  // The 3rd argument creates the group name.
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'ticket-created-queue-group',
+    options,
+  )
+
   subscription.on('message', (messageData: Message) => {
     // Message subject
-    console.log(messageData.getSubject());
+    console.log(messageData.getSubject())
     // Event number
     console.log(messageData.getSequence())
     // Message Data
     console.log(messageData.getData())
+
+    // We have received the message and processed it.
+    // Need to make this call b/c we set .setManualAckMode(true)
+    messageData.ack()
   })
+})
+
+// Kill the client when its down.
+// This prevents the kill delay by NATS
+process.on('SIGINT', () => {
+  stan.close()
+})
+process.on('SIGTERM', () => {
+  stan.close()
 })
--- src/publisher.ts
+++ src/publisher.ts
@@ -4,9 +4,8 @@ import nats from 'node-nats-streaming'
  * Connect to the Nats streaming service
  */
 const stan = nats.connect('ticketing', 'ticket-publisher', {
-  url: "http://localhost:4222"
+  url: 'http://localhost:4222',
 })
-
 
 /**
  * Once we are connected we will pass the mocked ticket
@@ -19,8 +18,8 @@ stan.on('connect', () => {
   const ticketData = JSON.stringify({
     id: 23746892736,
     title: 'Breaks not working right',
-    description: 'Client complains his breaks are squeaking.'
-  });
+    description: 'Client complains his breaks are squeaking.',
+  })
 
   // Publish event and data to the NATS streaming service
   stan.publish('ticket:created', ticketData, (err, guid) => {
